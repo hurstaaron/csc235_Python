0852da7e7b1b8cc2e142809d5496721e2ea2e171
hurstaaron <137419030+hurstaaron@users.noreply.github.com>
2025-11-01 19:21:47 -0700

    Add files via upload

---
+++ FirstApp.py
@@ -0,0 +1,41 @@
+# write a print statement in Python
+# create tutorial app in python
+    # include one python command
+    # legible and formatted correctly
+# this was fun and memorable
+
+
+print()
+print("Hello Everyone! Lets create a recipe application"
+      "for making bread!") # print command, hello world is a string of text, new line is created by
+print("Using Python print commands, here is how we display the recipe.") # discuss use of print commands
+print() # blank space
+# Instructions and ingredients
+print("Here is how to mix the ingredients.")
+print()
+print("+-----------------------------------------------------------+") # top boarder
+print("| 1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.|") # recipe part with boarder
+print("| 2. Knead the dough for 10 minutes.                        |") # recipe part with boarder
+print("| 3. Add 3g of Salt.                                        |") # recipe part with boarder
+print("| 4. Leave to rise for 2 hours.                             |") # recipe part with boarder
+print("| 5. Bake at 200 degrees C for 30 minutes.                  |") # recipe part with boarder
+print("+-----------------------------------------------------------+") # bottom boarder
+print() # blank space
+print() # blank space
+
+# making an alien with Python print statements
+print("Now, How about building an alien with some ASCII art?")
+print("Let create an alien with Python print commands")
+print()
+print("   /\\  /\\") # ears created, the escape sequence requires an extra '\' to tell the compiler there is a \
+print("  ( 0  0 )") # head and eyes
+print("   \\ o  /") # face and mouth, note the escape sequence
+print("  /| x |\\") # shoulders and upper arm, note escape sequence
+print(" / |   | \\") # lower arm, note escape sequence
+print("   |___|") # end of body
+print("   // \\\\") # stubby legs
+print()
+print("XynxoBoBo is a scary alien!") # name of alien
+print("He is from the Ghanndo Nebula") # where alien is from
+
+
